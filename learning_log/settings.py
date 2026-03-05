2b52d7c966507ce26c3dcc3e3c734fdfda1797cb
utterlyw <jallenkwara06@gmail.com>
2026-03-05 16:34:50 +0000

    Fix the debug to False in production

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['.onrender.com', 'localhost', '127.0.0.1']
 # shows the website addresses that can acess my site.
